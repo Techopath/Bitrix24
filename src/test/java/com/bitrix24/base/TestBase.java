bb8f3d443fdc4a30d3d975e97e33792699329953
Amir1994usa <amirburxanov94@gmail.com>
2020-04-15 12:45:43 -0400

    CommaIcon

--- src/test/java/com/bitrix24/base/TestBase.java
+++ src/test/java/com/bitrix24/base/TestBase.java
@@ -2,17 +2,20 @@ package com.bitrix24.base;
 
 import com.bitrix24.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase {
+public abstract class TestBase {
     protected WebDriver driver;
+    protected WebDriverWait wait;
     @BeforeMethod
     public void setDriver(){
         driver= Driver.getDriver();
+        wait= new WebDriverWait(driver,10);
     }
     @AfterMethod
     public void tearDown(){
-        Driver.closeDriver();
+    Driver.closeDriver();
     }
 }
