23ecd54b73df494dd25aeb11e056b736b6b299dd
oguzhandogan <o.dogan.9898@gmail.com>
2020-04-15 15:50:17 -0400

    oguzhandogan

--- src/test/java/com/bitrix24/base/TestBase.java
+++ src/test/java/com/bitrix24/base/TestBase.java
@@ -2,14 +2,19 @@ package com.bitrix24.base;
 
 import com.bitrix24.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import javax.swing.*;
+
 public class TestBase {
     protected WebDriver driver;
+    protected Actions action;
     @BeforeMethod
     public void setDriver(){
         driver= Driver.getDriver();
+        action = new Actions( driver );
     }
     @AfterMethod
     public void tearDown(){
