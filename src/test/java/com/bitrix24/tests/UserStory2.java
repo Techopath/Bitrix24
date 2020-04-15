70b1514a750a6abe680ee120f01a7419449fc3f7
Stacy1425 <nusya1425@gmail.com>
2020-04-15 15:27:30 -0400

    new Stacy commit

--- src/test/java/com/bitrix24/tests/UserStory2.java
+++ src/test/java/com/bitrix24/tests/UserStory2.java
@@ -2,4 +2,18 @@ package com.bitrix24.tests;
 
 public class UserStory2 {
 
+//2. As a user, I should be able to assign
+//    tasks by clicking on Task tab under Active Stream.
+/*
+"1. User should be able to click on ""High Priority"" checkbox to set the current task to a top priority task.
+2. User should be able to click on Visual Editor and see the editor text-bar displays on top of the message box.
+3. User should be able to click on upload files icon to upload files and pictures from local disks, download from external drive, select documents from bixtrix24, and create files to upload.
+4. User should be able to create a quote by clicking on the Comma icon.
+5. User should be able to add mention by clicking on the Add mention icon and select contacts from the lists provided in dropdown.
+6. User should be able to attach link by clicking on the link icon.
+7. User should be able to click on ""Checklist"" to create checklists items(Things to do). User can add a checklist item by clicking on add button or check mark. User can add separator lines between checklist items. User can delete a checklist item by clicking on x mark.
+8. User can assign the tasks to employees by clicking on Add More and selecting contact from E-mail user, Employees and Departments and Recent contact lists. Employees can be added in different assignment categories: Created By, Participants and Observer.
+9. User can add Deadline, Time Planningby using date pickers.
+10. User can click on More to specify the task details."
+ */
 }
