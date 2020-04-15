e4b38922f95ab0cab33ce26a98d3d310e920acbe
Edo <edogeda@yahoo.com>
2020-04-15 16:13:41 -0400

    first commit

---
+++ src/test/java/com/bitrix24/tests/Attachment.java
@@ -0,0 +1,9 @@
+package com.bitrix24.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Attachment {
+    WebDriver driver=new ChromeDriver();
+
+}
